4a6f37a63a704026c9ccd2eacf29a0cacb1dffad
Ask <prefuse@gmail.com>
2015-05-13 10:08:25 +0200

    Added Logan and Practice

---
+++ ArtOfUnitTesting/CH3.LogAn/LogAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn
+{
+    public class LogAnalyzer
+    {
+        private IExtensionManager manager;
+
+        // Constructor used to create seam where fake can be injected
+        public LogAnalyzer(IExtensionManager mgr)
+        {
+            manager = mgr;
+        }
+
+        // Unit of work under test:
+        public bool IsValidLogFileName(string fileName)
+        {
+            return manager.IsValid(fileName);
+        }
+    }
+
+    // Extracting an interface to break dependency
+    public interface IExtensionManager
+    {
+        bool IsValid(string fileName);
+    }
+
+    public class FileExtensionManager : IExtensionManager
+    {
+        public bool IsValid(string fileName)
+        {
+            return false; // placeholder
+        }
+    }
+}
