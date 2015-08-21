72219a39390230a363cb2607385b0f822706e039
LividLipid <prefuse@gmail.com>
2015-08-21 16:24:07 +0200

    ConsoleMenuTDD

---
+++ C#_practice/ConsoleMenu/ConsoleMenuTDD/MenuComponent.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleMenuTDD
+{
+    public abstract class MenuComponent
+    {
+
+
+        public string Title { get; }
+
+        public MenuComponent(string title)
+        {
+            Title = title;
+        }
+
+        public abstract void AddChild(MenuComponent child);
+        public abstract MenuComponent GetChild(int i);
+        public abstract void RemoveChild(int i);
+    }
+}
---
+++ C#_practice/ConsoleMenu/ConsoleMenuTDD/MenuComposite.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleMenuTDD
+{
+    public class MenuComposite : MenuComponent
+    {
+        private readonly List<MenuComponent> _children = new List<MenuComponent>();
+        public int ChildrenCount = 0;
+
+        public MenuComposite(string title) : base(title)
+        {
+        }
+
+        public override void AddChild(MenuComponent child)
+        {
+            _children.Add(child);
+            ChildrenCount++;
+        }
+
+        public override MenuComponent GetChild(int i)
+        {
+            return _children[i];
+        }
+
+        public override void RemoveChild(int i)
+        {
+            bool IsInRange = (i >= 0) && (i <= _children.Count-1);
+            if (IsInRange)
+            {
+                _children.RemoveAt(i);
+                ChildrenCount--;
+            }
+        }
+
+        public List<string> GetChildrenTitles()
+        {
+            var titles = _children.Select(child => child.Title).ToList();
+            return titles;
+        }
+    }
+}
